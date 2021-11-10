c727786a82747fa61792561337f9c6c524b419d2
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-10 17:51:49 -0500

    add test to date.test.js
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -8,5 +8,10 @@ test('should correctly create a date object with user inputted date', () => {
   const date = new Date("1983-07-20");
   expect(date.userDate).toEqual("1983-07-20");  
   });
+
+test('should correctly return the day of the week based on user inputted date', () => {
+  const date = new Date("1983-07/20");
+  expect(date.getDay).toEqual("Wednesday");
+  })  
 });
 
